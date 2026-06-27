c37b02699899b0595f93a4483859250c8a916994
ferozshaik9912 <FEROZSHAIK3800@GMAIL.COM>
2026-06-27 17:32:08 +0530

    Time: 0 ms (100%), Space: 19.5 MB (52.84%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fizzBuzz(self,n):
+        list=[]
+        for i in range(1,n+1):
+            if i%3==0 and i%5==0:
+                result="FizzBuzz"
+            elif i%3==0:
+                result="Fizz"
+            elif i%5==0:
+                result="Buzz"
+            else:
+                result= str(i)
+            list.append(result)
+        return list
+        
